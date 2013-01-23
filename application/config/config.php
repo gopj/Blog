2a643babea48141bcdc0465af10ccd79cd6708cd
Jesús González <gopixc@gmail.com>
2013-01-23 13:44:19 -0600

    jQuery + Bootstrap

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/GitHub/prueba/';
+$config['base_url']	= 'http://localhost/GitHub/Blog/';
 
 /*
 |--------------------------------------------------------------------------
